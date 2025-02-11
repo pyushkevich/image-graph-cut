a986d8fd8ab33eba4d444b77cc72185942cc6657
Paul Yushkevich <pyushkevich@gmail.com>
2025-02-11 12:19:55 -0500

    build fixes

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -3,6 +3,19 @@ set -x -e
 
 mkdir -p be/install && cd be
 
+# Build METIS
+git clone https://github.com/KarypisLab/METIS.git METIS
+cmake \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=./install \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -B METIS/build \
+    METIS
+
+cmake --build METIS/build --target install $MAKEFLAGS
+
 # Build ITK
 git clone -b v5.4.0 https://github.com/InsightSoftwareConsortium/ITK.git ITK
 cmake \
